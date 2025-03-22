3c8b7723563f57c30d88516bee303c6917c84cc8
Julio Duarte <duartejulio2016@gmail.com>
2025-03-22 20:04:30 -0300

    ✨ feat:Added  file config for environement dev

--- src/main/java/com/piunivespg4/encantometrobackend/EncantometrobackendApplication.java
+++ src/main/java/com/piunivespg4/encantometrobackend/EncantometrobackendApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@Profile("dev")
 public class EncantometrobackendApplication {
 
 	public static void main(String[] args) {
