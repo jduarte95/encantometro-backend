a5c6639cb736bd2a3b7c1847c300d9c1078e4a90
Julio Duarte <duartejulio2016@gmail.com>
2025-03-22 19:19:32 -0300

    ✨ feat:Added project scaffolding for springboot

---
+++ src/main/java/com/piunivespg4/encantometrobackend/EncantometrobackendApplication.java
@@ -0,0 +1,13 @@
+package com.piunivespg4.encantometrobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncantometrobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncantometrobackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piunivespg4/encantometrobackend/EncantometrobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piunivespg4.encantometrobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncantometrobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
